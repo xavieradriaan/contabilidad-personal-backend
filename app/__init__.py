74b26b51a913062ed7cca5f09f2aa1c6d330d155
Xavier Andrade <xavierandrade@MacBook-Pro-de-Xavier.local>
2024-11-11 22:26:12 -0500

    ppp

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 #__init__.py
+#__init__.py
 import atexit
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -12,9 +13,9 @@ from flask_migrate import Migrate
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xavier:1234@localhost/contabilidad_personal'
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_secret_key')
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'your_jwt_secret_key')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 api = Api(app)
