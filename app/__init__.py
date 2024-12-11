572976283803570e674128965d38123eddeb7126
Xavier Andrade <xavierandrade@MacBook-Pro-de-Xavier.local>
2024-12-11 18:24:11 -0500

    safsaf

--- app/__init__.py
+++ app/__init__.py
@@ -13,10 +13,10 @@ import os
 from datetime import timedelta
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_secret_key')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xavier:1234@localhost/contabilidad_personal'
+app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'your_jwt_secret_key')
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=40)  # Expiración del token en 40 minutos
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=40)  # Expiración del token en 5 minutos
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
