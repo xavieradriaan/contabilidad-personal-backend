13afd38b3f3b9d8f04aea51a58f5146035a93b3e
Xavier Andrade <xavierandrade@MacBook-Pro-de-Xavier.local>
2024-12-18 10:48:43 -0500

    mismo del anterior commit

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ from datetime import timedelta
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xavier:1234@localhost/contabilidad_personal'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_secret_key')
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'your_jwt_secret_key')
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=40)
